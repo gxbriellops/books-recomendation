a9234747dcb13549d44d563dd67362d22c57f489
Gabriel Lopes <gabriellopes.ct07@gmail.com>
2025-02-25 15:02:02 -0300

    feat: visual & collumns

--- app.py
+++ app.py
@@ -1,13 +1,56 @@
 import pickle
 import numpy as np
-import os
 import streamlit as st
+import pandas as pd
+from math import floor
+
+def star_print(star):
+    no_star = ' '
+    star_moji = '⭐'
+    star = star / 2
+    star = floor(star)
+    if star == 0:
+        st.text(no_star)
+    else:
+        st.text(star_moji * star)
+
 
 st.header('Meu primeiro webapp usando Machine Learning')
 model = pickle.load(open('artefatos/model.pkl', 'rb'))
 book_name = pickle.load(open('artefatos/book_name.pkl', 'rb'))
 book_pivot = pickle.load(open('artefatos/book_pivot.pkl', 'rb'))
 final_data = pickle.load(open('artefatos/final_data.pkl', 'rb'))
+df = pd.read_csv('archive/final_data.csv')
+st.write(' ')
+
+st.subheader('Os mais vendidos')
+
+cinco_mais_vendidos = df.sort_values(by='rating', ascending=False).head(5).sort_values(by='num_rating', ascending=False)
+titles = []
+urls = []
+stars = []
+num_ratings = []
+
+for index, row in cinco_mais_vendidos.iterrows():
+    book_names = row['title']
+    titles.append(book_names)
+    url = row['image_url']
+    urls.append(url)
+    star = row['rating']
+    stars.append(star)
+    num_rating = row['num_rating']
+    num_ratings.append(num_rating)
+
+books_cols = st.columns(5)
+
+for i in range(5):
+    with books_cols[i]:
+        st.image(urls[i], width=300)
+        st.text(titles[i])
+        star_print(stars[i])
+        st.caption(f'{num_ratings[i]} avaliações')
+
+st.divider()
 
 selected_book = st.selectbox(
     'Escolha um livro',
@@ -43,24 +86,14 @@ def recomendation(title):
 
 if st.button('Mostrar Recomendações'):
     recomendation_books, poster_url = recomendation(selected_book)
-    col1, col2, col3, col4, col5 = st.columns(5)
-
-    with col1:
-        st.text(recomendation_books[1])
-        st.image(poster_url[1])
-
-    with col2:
-        st.text(recomendation_books[2])
-        st.image(poster_url[2])
-        
-    with col3:
-        st.text(recomendation_books[3])
-        st.image(poster_url[3])
-
-    with col4:
-        st.text(recomendation_books[4])
-        st.image(poster_url[4])
-
-    with col5:
-        st.text(recomendation_books[5])
-        st.image(poster_url[5])
+    col_recomdation = st.columns(5)
+    for book_rec in range(5):
+        with col_recomdation[book_rec]:
+            st.image(poster_url[book_rec], width=300)
+            st.text(recomendation_books[book_rec])
+            book_info = final_data.loc[final_data['title'] == recomendation_books[book_rec]]
+            if not book_info.empty:
+                star_rec = book_info['rating'].values[0]
+                n_avaliacoes_rec = book_info['num_rating'].values[0]
+                star_print(star_rec)
+                st.caption(f'{n_avaliacoes_rec} avaliações')
